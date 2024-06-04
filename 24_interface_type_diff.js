bd85255e627c7b76a479ee7b9e4eb27cf2c82b89
illahibuxjagirani <illahibuxjagirani@gmail.com>
2024-06-04 11:47:44 +0500

    new

---
+++ 24_interface_type_diff.js
@@ -0,0 +1,9 @@
+// type and interface differnece
+var myObj = {
+    name: "Ali",
+    age: 18
+};
+console.log(myObj);
+// 4. Usage;
+// interface typically used to define shape of an object
+// type can be used for object types, union types, primitive types, typles, etc.
